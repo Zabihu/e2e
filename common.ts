83891031a1cfbec5229c93d6fe1473dd652c8dbe
Zabi <zabipro@gmail.com>
2021-04-08 11:59:06 +0530

    Update common.ts

--- common.ts
+++ common.ts
@@ -2,6 +2,6 @@
     async getRecordRowNo(recordName: string, index = 1) {
         let recordRowXpath = "(//td[contains(.,'" + recordName + "')]/ancestor::tr)[" + index + "]";
         let recordRowElement = await element(by.xpath(recordRowXpath)).getAttribute('class');
-        let recordRowNo = recordRowElement.match(/row-([0-9]+)/gi)[0].replace('row-', "");
+        let recordRowNo = recordRowElement.match(/row-([0-9]+)/)[0].replace('row-', "");
         return Number(recordRowNo);
     }
